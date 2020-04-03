a518e94d154b0de1f5363d997c827c5dc5421c76
kita0402 <goro0410@gmail.com>
2020-04-03 10:09:04 +0000

    指定勤務開始＆終了時間追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200403093354) do
+ActiveRecord::Schema.define(version: 20200403095820) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -32,10 +32,12 @@ ActiveRecord::Schema.define(version: 20200403093354) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-03-26 23:00:00"
-    t.datetime "work_time", default: "2020-03-26 22:30:00"
+    t.datetime "basic_time", default: "2020-04-02 23:00:00"
+    t.datetime "work_time", default: "2020-04-02 22:30:00"
     t.integer "employee_number"
     t.integer "uid"
+    t.datetime "designated_work_start_time", default: "2020-04-03 00:00:00"
+    t.datetime "designated_work_end_time", default: "2020-04-03 09:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
